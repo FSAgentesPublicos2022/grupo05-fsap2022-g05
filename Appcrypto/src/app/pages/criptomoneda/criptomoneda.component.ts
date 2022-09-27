f14d885d089c01eae39a734f3b0e793c314a9f7b
MelisaVilar <105024915+MelisaVilar@users.noreply.github.com>
2022-09-27 20:20:04 -0300

    actualizacion

---
+++ Appcrypto/src/app/pages/criptomoneda/criptomoneda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criptomoneda',
+  templateUrl: './criptomoneda.component.html',
+  styleUrls: ['./criptomoneda.component.css']
+})
+export class CriptomonedaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Appcrypto/src/app/pages/iniciarsesion/iniciarsesion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iniciarsesion',
+  templateUrl: './iniciarsesion.component.html',
+  styleUrls: ['./iniciarsesion.component.css']
+})
+export class IniciarsesionComponent implements OnInit {
+  form: any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
