ed4ea38d07ffb6f7b92214cebbd89c454b1e56c4
MelisaVilar <105024915+MelisaVilar@users.noreply.github.com>
2022-10-04 20:12:02 -0300

    Update app-routing.module.ts
    MODIFICACION PAG 404

--- Appcrypto/src/app/app-routing.module.ts
+++ Appcrypto/src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { OperacionesComponent } from './pages/operaciones/operaciones.component';
 import { TransaccionesComponent } from './pages/transacciones/transacciones.component';
 import { CriptomonedaComponent } from './pages/criptomoneda/criptomoneda.component';
-import { Pagina404Component } from './pages/pagina404/pagina404.component';
 
 const routes: Routes = [
   {path:'servicios',component:ServiciosComponent},
@@ -22,8 +21,6 @@ children:[
 {path:'operaciones', component: OperacionesComponent},
 {path:'transacciones', component: TransaccionesComponent},
 {path:'criptomoneda', component: CriptomonedaComponent},
-
-{path: '**', component: Pagina404Component}
 ]}
 ]
 
